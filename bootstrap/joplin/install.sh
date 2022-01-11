2c2fc35b9abb36ec1598705b855b9dba50f5a736
iFork <tools@logreg.e4ward.com>
2022-01-11 23:41:11 +0100

    CHORE(bootstrap): joplin installation

--- bootstrap/joplin/install.sh
+++ bootstrap/joplin/install.sh
@@ -4,7 +4,13 @@
 set -o nounset    # error when referencing undefined variable
 set -o errexit    # exit when command fails
 
-# Install joplin desktop app
+# Install joplin desktop app {{{1
+# General Install script from [Joplin - an open source note taking and to-do application with synchronisation capabilities](https://joplinapp.org/help/#installation)
+# installs appimage
+echo 'Installing Joplin desktop'
+wget -O - https://raw.githubusercontent.com/laurent22/joplin/dev/Joplin_install_and_update.sh | bash
+
+# Following (bash) `command` test may not work for appimages.
 if [ ! -x "$(command -v joplin-desktop)" ]; then
     echo 'Installing Joplin desktop'
     wget -O - https://raw.githubusercontent.com/laurent22/joplin/master/Joplin_install_and_update.sh | bash
@@ -20,7 +26,7 @@ if [ ! -x "$(command -v node)" ]; then
     #see also: https://github.com/nodesource/distributions/blob/master/README.md
 fi
 
-# Install joplin terminal app
+# Install joplin terminal app {{{1
 if [ ! -x "$(command -v joplin)" ]; then
     echo 'Installing Joplin terminal'
     NPM_CONFIG_PREFIX=~/.joplin-bin npm install -g joplin
